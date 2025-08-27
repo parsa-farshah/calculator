9c4bd54985a74c112324c9a985977c0e80bcd7e3
parsa <parsafarashah@gmail.com>
2025-08-28 02:15:15 +0330

    found bug

--- src/js/master.js
+++ src/js/master.js
@@ -94,13 +94,13 @@ btnLight.addEventListener("click", () => {
   txtColor.forEach((el) => el.classList.add("text-white"));
 });
 
-let calc = document.getElementById("calc");
+// let calc = document.getElementById("calc");
 
-function anim() {
-  calc.classList.toggle("rotate-z-[60deg]");
-  calc.classList.toggle("rotate-x-[40deg]");
-  calc.classList.toggle("sm:top-7");
-  calc.classList.toggle("top-[80px]");
-  calc.classList.toggle("left-[50%]");
-  calc.classList.toggle("left-[55%]");
-}
+// function anim() {
+//   calc.classList.toggle("rotate-z-[60deg]");
+//   calc.classList.toggle("rotate-x-[40deg]");
+//   calc.classList.toggle("sm:top-7");
+//   calc.classList.toggle("top-[80px]");
+//   calc.classList.toggle("left-[50%]");
+//   calc.classList.toggle("left-[55%]");
+// }
